b3fd21b5688f099b11a6065e0e5d9ad503364e63
Emmanuel Tweneboah <twentekghana@gmail.com>
2020-03-19 11:22:06 +0800

    Navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
+import NavbarDashboard from "./components/Navbar/NavbarDashboard/NavbarDashboard";
 
 function App() {
   return (
     <BrowserRouter>
+      <NavbarDashboard />
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
