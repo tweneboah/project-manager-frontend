02d8d696243366acda85e035d60972c40bbc873b
Emmanuel Tweneboah <twentekghana@gmail.com>
2020-03-19 11:04:59 +0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
