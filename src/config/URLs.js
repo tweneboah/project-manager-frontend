d730da7fd0ffe40b1094e2cac921c26bab468b17
Emmanuel Tweneboah <twentekghana@gmail.com>
2020-03-20 17:40:58 +0800

    API

--- src/config/URLs.js
+++ src/config/URLs.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://teklinco-project-manager.herokuapp.com";
+export const API_URL = "https://project-manager-api-v1.herokuapp.com";
 
 // export const API_URL = "http://localhost:1337";
